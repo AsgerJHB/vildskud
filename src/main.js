b0bcfd5360bdb5670fb98099ed528a445d216ec5
Jonas Krogh Fischer <jonasfiskekrogh@hotmail.com>
2022-10-09 13:47:02 +0200

    hero sektion med table-program

--- src/main.js
+++ src/main.js
@@ -1,20 +1,42 @@
 import "./style.css";
+document.getElementById("dageDropdown").addEventListener("change", filterTable);
 
-const info1 = document.getElementById("info1");
-const info2 = document.getElementById("info2");
-const info3 = document.getElementById("info3");
-const btn1 = document.getElementById("togglebtn1");
-const btn2 = document.getElementById("togglebtn2");
-const btn3 = document.getElementById("togglebtn3");
+function filterTable() {
+  console.log("hej");
+  // Variabler
+  let dropdown, table, rows, cells, dag, filter;
+  dropdown = document.getElementById("dageDropdown");
+  table = document.getElementById("myTable");
+  rows = table.getElementsByTagName("tr");
+  filter = dropdown.value;
 
-btn1.addEventListener("click", (e) => {
-  info1.classList.toggle("hidden");
-});
+  // Filtrer med dropdown menuen
+  for (let row of rows) {
+    cells = row.getElementsByTagName("td");
+    dag = cells[3] || null; // rammer den tredje "td" i "tr"
+    if (filter === "Dato" || !dag || filter === dag.textContent) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  }
+}
 
-btn2.addEventListener("click", (e) => {
-  info2.classList.toggle("hidden");
-});
-
-btn3.addEventListener("click", (e) => {
-  info3.classList.toggle("hidden");
+//fold rækker ud og roter pil
+document.querySelectorAll(".table_row").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    {
+      let infotekst = item.nextElementSibling;
+      let arrowTing = item.lastElementChild;
+      if (infotekst.style.display === "none") {
+        infotekst.style.display = "block";
+        arrowTing.classList.add("arrow");
+        arrowTing.classList.remove("arrow2");
+      } else {
+        infotekst.style.display = "none";
+        arrowTing.classList.remove("arrow");
+        arrowTing.classList.add("arrow2");
+      }
+    }
+  });
 });
